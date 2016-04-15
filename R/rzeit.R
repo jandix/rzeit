bb8ba321b3a377ae3f6003ca7f65400f883797cd
chgrl <christian.graul@gmail.com>
2016-04-15 20:36:03 +0200

    package help page added

---
+++ R/rzeit.R
@@ -0,0 +1,15 @@
+#'@title Client for the ZEIT ONLINE Content API
+#'@description Interface to gather newspaper articles from ZEIT ONLINE, based on a 
+#'multilevel query. Including sorting algorithms and graphical output options.
+#'A personal API key is required for usage.
+#'@name rzeit
+#'@docType package
+#'@references \url{http://developer.zeit.de}
+#'@seealso \code{\link{fromZeit}} to expose a search in the ZEIT online archive,
+#'\code{\link{zeitClient}} to get client information,
+#'\code{\link{zeitFrequencies}} to get a frequency table of the search results,
+#'\code{\link{zeitPlot}} to plot results of a search,
+#'\code{\link{zeitSetApiKey}} to set the API Key to the environment,
+#'\code{\link{zeitToDf}} to get a data frame with sorted articles
+#'@aliases rzeit rzeit-package
+NULL
